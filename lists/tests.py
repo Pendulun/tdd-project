3c01960714ecfb28ff6c020580479be641678142
Pendulun <danielcampos14@hotmail.com.br>
2021-02-25 20:12:32 -0300

    Salvando o POST no Banco de Dados

--- lists/tests.py
+++ lists/tests.py
@@ -2,14 +2,18 @@ from django.test import TestCase
 from lists.models import Item
 
 class HomePageTest(TestCase):
-	def test_home_page_returns_correct_html(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
 class ItemModelTest(TestCase):
 
