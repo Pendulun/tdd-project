d4d162ef9f3ca9a04086c0314ff12c45d91f46be
Pendulun <danielcampos14@hotmail.com.br>
2021-03-01 18:23:22 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists import views
+from lists import views	
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
